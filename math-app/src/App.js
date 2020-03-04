c12c5a95235c8a1f9ed5b3cfc568f9d1656541ff
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-03-04 07:39:28 -0800

    added front end app

---
+++ math-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span className="App-logo" alt="logo"><p>+</p></span>
+        <h1>Math Game</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
