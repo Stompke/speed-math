f32ec65517910b539bddf92c18a0a63485a593c6
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-03-11 08:26:24 -0700

    missing email username and short password alert added

--- users/router.js
+++ users/router.js
@@ -23,53 +23,59 @@ router.post('/', (req, res) => {
 
     const hash = bcrypt.hashSync(password, 8);
     user.password = hash;
+    if(!email || !username) {
+        res.status(401).json({ message: "Username and password are required."})
+    } else if (password.length < 8) {
+        res.status(401).json({ message: "Password must be at least 8 characters."})
+    } else {
 
-
-    Users.findBy({email})
-    .first()
-    .then(userEmail => {
-        Users.findBy({username})
+        Users.findBy({email})
         .first()
-        .then(userUsername => {
-            if(userEmail) {
-                res.status(401).json({ message: "Email already taken."})
-            } else if(userUsername) {
-                res.status(401).json({ message: "Username already taken."})
-            } else {
-                Users.add(user)
-                .then(users => {
-                    Users.findBy({email})
-                    .first()
-                    .then(user => {
-                        console.log(user)
-                        if( user && bcrypt.compareSync(password, user.password)) {
-                            res.status(200).json({
-                                id: user.id,
-                                message: `Welcome ${user.username}`,
-                                token: generateToken(user)
-                            })
-                        } else {
-                            res.status(400).json({ message: "Invalid Credentials"})
-                        }
+        .then(userEmail => {
+            Users.findBy({username})
+            .first()
+            .then(userUsername => {
+                if(userEmail) {
+                    res.status(401).json({ message: "Email already taken."})
+                } else if(userUsername) {
+                    res.status(401).json({ message: "Username already taken."})
+                } else {
+                    Users.add(user)
+                    .then(users => {
+                        Users.findBy({email})
+                        .first()
+                        .then(user => {
+                            console.log(user)
+                            if( user && bcrypt.compareSync(password, user.password)) {
+                                res.status(200).json({
+                                    id: user.id,
+                                    message: `Welcome ${user.username}`,
+                                    token: generateToken(user)
+                                })
+                            } else {
+                                res.status(400).json({ message: "Invalid Credentials"})
+                            }
+                        })
+                        .catch(err => {
+                            res.status(500).json({ error: "Could not login", err})
+                        })
                     })
                     .catch(err => {
-                        res.status(500).json({ error: "Could not login", err})
+                        res.status(500).json({ error: "Could not Register User"})
                     })
-                })
-                .catch(err => {
-                    res.status(500).json({ error: "Could not Register User"})
-                })
+        
+                }
     
-            }
-
+            })
+            .catch(err => {
+                res.status(500).json({ error: "Could not search through usernames", err})
+            })
         })
         .catch(err => {
-            res.status(500).json({ error: "Could not search through usernames", err})
+            res.status(500).json({ error: "Could not search through emails", err})
         })
-    })
-    .catch(err => {
-        res.status(500).json({ error: "Could not search through emails", err})
-    })
+    }
+
 
 })
 
